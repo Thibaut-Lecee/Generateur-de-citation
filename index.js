1977ce784eedd4e1978671c617b89d6b127b69cf
Thibaut-Lecee <81172998+Thibaut-Lecee@users.noreply.github.com>
2021-03-31 21:17:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,96 @@
 
-// une variable pour récuperer depuis mon fichier html, le composant qui porte l'id 'nom'
-const url = "http://graven.yt/citations.json";
+ // une variable pour récuperer depuis mon fichier html, le composant qui porte l'id 'nom'
+
 let btn = document.getElementById('btn');
-let avatar = document.getElementById('avatar'); 
+let avatar = document.getElementById('avatar');
 let citation = document.getElementById('citation');
 let text = document.getElementById('nom');
-let citations = [];
+let citations = [
+    {
+
+        "nom": "Bill gates",
+
+        "citation": "Le logiciel est une excellente combinaison entre l'art et l'ingénierie.",
+
+    	"image": "http://graven.yt/avatar/billgates.jpg"
+
+        
+    },
+    {
+
+        "nom": "Bjarne Stroustrup",
+
+        "citation": "Il n'y a que deux sortes de langages de programmation: ceux dont les gens disent toujours du mal et ceux que personne n'utilise.",
+
+    	"image": "http://graven.yt/avatar/bjarne.jpg"
+
+    },
+    {
+
+    	"nom": "Edsger Dijkstra",
+
+    	"citation": "Si debugger, c’est supprimer des bugs, alors programmer ne peut être que les ajouter",
+
+    	"image": "http://graven.yt/avatar/edsger.jpg"
+
+    },
+
+    {
+
+        "nom": "Mark Zuckerberg",
+
+        "citation": "Une règle simple pour vraiment changer les choses, c’est de commencer toujours par le plus simple, et non par le plus difficile. Parce que c’est la meilleure manière de progresser rapidement.",
+
+    	"image": "http://graven.yt/avatar/zuckerberg.jpg"
+
+    },
+
+    {
+
+    	"nom": "Toby Parkins",
+
+    	"citation": "La refactorisation c’est comme faire la vaisselle après la cuisine.",
+
+    	"image": "http://graven.yt/avatar/toby.jpg"
+
+    },
+
+    {
+
+        "nom": "Auteur inconnu",
+
+        "citation": "Le fossé séparant théorie et pratique est moins large en théorie qu’il ne l’est en pratique",
+
+    	"image": "http://graven.yt/avatar/inconnu.jpg"
+
+    },
+
+    {
+
+        "nom": "Alain Turing",
+
+        "citation": "Les tentatives de création de machines pensantes nous seront d'une grande aide pour découvrir comment nous pensons nous-mêmes.",
+
+    	"image": "http://graven.yt/avatar/turing.jpg"
+
+    }
+
+
+
+];
 
 // faire l'evenement lors d'un click bouton
 btn.addEventListener('click', updatePage);
 
 // recuperer toutes les citations depuis le lien graven.yt/citations.json
-fetch(url).then((data) => {
-    data.json().then((data) => {
-        citations = data;
+//fetch(url).then((data) => {
+  //  data.json().then((data) => {
+    //    citations = data;
 
         // affichage de citations
-        console.log(citations);
-    });
-});
+      //  console.log(citations);
+    //});
+//});
 
 function updatePage() {
  
